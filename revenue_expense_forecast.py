b7bdcc8fcf272697acdb1153b11ebc4648bf8ef2
Alex Cohen <alexcohen4080@gmail.com>
2022-11-04 14:26:24 -0400

    test commit

---
+++ revenue_expense_forecast.py
@@ -0,0 +1,44 @@
+import fetch_data as fd
+import pandas as pd
+import numpy as np
+import datetime
+
+
+def generate_dataframe(symbol):
+    # generate index and columns for rev exp forecast dataframe
+    df_index = [
+        'Revenue', '% Growth',
+        'COGS', '% of Revenue',
+        'SG&A', '% of Revenue',
+        'R&D', '% of Revenue',
+        'Net Interest', '% of Revenue',
+        'Other income/expenses', '% of Revenue',
+        'Tax Expense', 'Tax Rate',
+        'Net Profit', '% Margin'
+    ]
+    df_columns = []
+    current_year = datetime.date.today().year
+    # TODO: variable adjust for how far out to forecast
+    for year in range(0, 6):
+        df_columns.append(current_year + year)
+
+    rev_exp_forecast = pd.DataFrame(index=df_index, columns=df_columns)
+    return rev_exp_forecast
+
+
+def fill_recent_year(df, symbol):
+    # retrieve financial statement
+    balance_sheet = fd.get_balance_sheet(symbol)
+    income_statement = fd.get_income_statement(symbol)
+    cash_flows = fd.get_cash_flows(symbol)
+    year = datetime.date.today().year
+
+    df.at['Revenue', year] = income_statement['totalRevenue']
+    df.at['COGS', year] = income_statement['costOfRevenue']
+    df.at['SG&A', year] = income_statement['sellingGeneralAndAdmin']
+    df.at['R&D', year] = income_statement['researchAndDevelopment']
+    df.at['Net Interest', year] = income_statement['interestIncome']
+    df.at['Other income/expenses', year] = income_statement['otherIncomeExpenseNet']
+    df.at['Tax Expense', year] = income_statement['incomeTax']
+    df.at['Net Profit', year] = income_statement['netIncome']
+    return df
