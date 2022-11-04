45730ca575dfc4e1a8c96e1e712e570eab3f947b
Alex Cohen <alexcohen4080@gmail.com>
2022-11-04 08:27:39 -0400

    test commit

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import numpy as np
-import pandas
 import pandas as pd
 import matplotlib.pyplot as plt
 import requests
 from datetime import datetime
-import fetch_data as hp
+import fetch_data as fd
 
 
 if __name__ == '__main__':
-    prices = hp.get_price_history('SPY')
-    prices = hp.remove_date_intervals(prices, 10)
-    cash_flows = hp.get_cash_flows('AAPL')
-    income_statement = hp.get_income_statement('AAPL')
-    balance_sheet = hp.get_balance_sheet('AAPL')
+    prices = fd.get_price_history('SPY')
+    prices = fd.remove_date_intervals(prices, 10)
+    cash_flows = fd.get_cash_flows('AAPL')
+    income_statement = fd.get_income_statement('AAPL')
+    balance_sheet = fd.get_balance_sheet('AAPL')
     print(cash_flows)
     print(income_statement)
     print(balance_sheet)
